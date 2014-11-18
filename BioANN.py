2b02a0da72241a9084c2ffd6dc62197fbe1886a7
jack <jack.e.holland.gr@dartmouth.edu>
2014-11-17 22:14:21 -0500

    Added error plotting functionality and the resulting image, error.png

--- BioANN.py
+++ BioANN.py
@@ -2,7 +2,6 @@ import numpy as np
 #pybrain stuff
 import pybrain
 from pybrain.datasets	import ClassificationDataSet
-
 from pybrain.utilities           import percentError
 from pybrain.tools.shortcuts     import buildNetwork
 from pybrain.supervised.trainers import BackpropTrainer
@@ -10,16 +9,14 @@ from pybrain.structure.modules   import SigmoidLayer
 from pybrain.structure.modules   import SoftmaxLayer
 from pybrain.tools.customxml import NetworkWriter
 from pybrain.tools.customxml import NetworkReader
-
 import random
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import sys
-
 import json
 from scipy import array, where, argmax
-
 import matplotlib.pyplot as plt
+import re
 
 #Default parameters
 micro_dim = 24 #the number of input nodes
@@ -35,10 +32,10 @@ myError = 10.0
 
 max_iterations = 1000000 #maximum number of iterations
 num_epochs = 5 #number of ephochs at every iteration
-snapshot = 1000 #when to snapshot the ANN
+snapshot = 10 #when to snapshot the ANN
 
 def load_params():
-	with open("params", 'r') as f_in:
+	with open('params', 'r') as f_in:
 		params = json.load(f_in)
 
 	global micro_dim
@@ -50,15 +47,15 @@ def load_params():
 	global max_iterations
 	global num_epochs
 	global snapshot
-	micro_dim = int(params["micro_dim"]) 
-	num_hidden = params["num_hidden"]
-	inc_bias = bool(params["inc_bias"])
+	micro_dim = int(params['micro_dim']) 
+	num_hidden = params['num_hidden']
+	inc_bias = bool(params['inc_bias'])
 
-	momentum = float(params["momentum"])
-	weight_decay = float(params["weight_decay"])
-	max_iterations = int(params["max_iterations"])
-	num_epochs = int(params["num_epochs"])
-	snapshot = int(params["snapshot"])
+	momentum = float(params['momentum'])
+	weight_decay = float(params['weight_decay'])
+	max_iterations = int(params['max_iterations'])
+	num_epochs = int(params['num_epochs'])
+	snapshot = int(params['snapshot'])
 
 
 
@@ -71,85 +68,70 @@ def extract_data(filename, ds):
 			microarray = [float(i) for i in cells[3:]]
 			ds.addSample(microarray, (label))
 
-def restart(filename, netfile):
+def train(filename, start=''):
 	load_params()
 	#import dataset
 	ds = ClassificationDataSet(micro_dim, 1, nb_classes=num_classes)
 	extract_data(filename, ds)
-
-	tr, val = ds.splitWithProportion(0.10) #10% validation data
-	#softmax output layer
-	tr._convertToOneOfMany()
-	val._convertToOneOfMany()
-
-	ann = NetworkReader.readFrom(netfile)
-	#restart training TODO
-	done = False
-	temp = netfile.split("_")
-
-	iteration = int(temp[0])
-	trainer = BackpropTrainer(ann,	dataset=tr, momentum=momentum, verbose=True, weightdecay=weight_decay)
-
-	while(not done):
-		trainer.trainEpochs(num_epochs)
-		error = percentError(trainer.testOnClassData(), tr['class'])
-		print "iter %d, error=%d" % (iteration, error)
-		if iteration >= max_iterations:
-			done = True
-		#pickle every 5 iterations
-		if iteration%snapshot == 0:
-			print "Saving model %d_%d.xml..."%(iteration, int(error))
-			NetworkWriter.writeToFile(ann, "%d_%d.xml"%(iteration, int(error)))
-
-		iteration = iteration + 1
-
-	#testing
-	tstresult = percentError(trainer.testOnClassData(dataset=val), val['class'])
-	print tstresult
-
-def train(filename):
-	load_params()
-	#import dataset
-	ds = ClassificationDataSet(micro_dim, 1, nb_classes=num_classes)
-	extract_data(filename, ds)
-
+	
 	tr, val = ds.splitWithProportion(0.9) #10% validation data
 	#softmax output layer
 	tr._convertToOneOfMany()
 	val._convertToOneOfMany()
-
+	
 	#build network
-	ann = buildNetwork(tr.indim, num_hidden[0], num_hidden[1], tr.outdim, hiddenclass=SigmoidLayer, recurrent=False, outclass=SoftmaxLayer, bias=inc_bias)
-
+	if start == '':
+		ann = buildNetwork(tr.indim, num_hidden[0], num_hidden[1], tr.outdim, hiddenclass=SigmoidLayer, recurrent=False, outclass=SoftmaxLayer, bias=inc_bias)
+		iteration = 0
+	else:
+		ann = NetworkReader.readFrom(start)
+		match = re.search('([0-9]+)_(?:[0-9]{1,3}).xml', start)
+		if match == None:
+			print 'Net save files should be named I_E.xml, where I is the iteration and E is the rounded error from 0-100'
+			exit(1)
+		else:
+			iteration = int(match.group(1)) + 1
+	print(iteration, type(iteration))
+	
 	#training 
 	trainer = BackpropTrainer(ann,	dataset=tr, momentum=momentum, weightdecay=weight_decay, verbose=True)
 	done = False
-	iteration = 0
 	errors, confidences = [], []
-
+	
 	while(not done):
 		trainer.trainEpochs(num_epochs)
 		errors.append(calcError(trainer))
 		confidences.append(calcConfidence(trainer))
-		print "iter %d, error %f, confidence %f" % (iteration, errors[-1], confidences[-1])
+		print 'iter %d, error %f, confidence %f' % (iteration, errors[-1], confidences[-1])
 
 		if iteration >= max_iterations - 1:
 			done = True
+		
 		#pickle every 5 iterations
-		#if iteration%snapshot == 0:
-			#print "Saving model %d_%d.xml..."%(iteration, int(error))
-			#NetworkWriter.writeToFile(ann, "%d_%d.xml"%(iteration, int(error)))
-			#print error
-
+		if iteration % snapshot == 0:
+			file_data = (iteration, int(errors[-1]*100))
+			print 'Saving model %d_%d.xml...' % file_data
+			NetworkWriter.writeToFile(ann, '%d_%d.xml' % file_data)
+		
 		iteration = iteration + 1
-
+	
 	#testing
 	print 'error %f, confidence %f' % (calcError(trainer, dataset=val), calcConfidence(trainer, dataset=val))
 	
 	#plotting
-	plt.plot(range(max_iterations), errors)
-	plt.plot(range(max_iterations), confidences)
-	plt.save('error.png')
+	iterations = range(max_iterations)
+	fig, ax1 = plt.subplots()
+	ax1.plot(iterations, errors, 'b-')
+	ax1.set_xlabel('iteration')
+	ax1.set_ylabel('mean squared error')
+	for tick in ax1.get_yticklabels():
+		tick.set_color('b')
+	ax2 = ax1.twinx()
+	ax2.plot(iterations, confidences, 'r-')
+	ax2.set_ylabel('confidence (L1 error)')
+	for tick in ax2.get_yticklabels():
+		tick.set_color('r')
+	plt.savefig('error.png')
 
 def calcError(trainer, dataset=None):
     if dataset == None:
@@ -182,8 +164,8 @@ def calcConfidence(trainer, dataset=None):
 
 if __name__ == '__main__':
 	operation = sys.argv[1]
-	if operation in "train":
+	if operation in 'train':
 		train(sys.argv[2])
 	else:
-		restart(sys.argv[2], sys.argv[3])
+		train(sys.argv[2], sys.argv[3])
 
