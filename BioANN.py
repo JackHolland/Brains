200aeb39aeb47c5fb398dd2c3b40bf2ab4e6ff8a
jack <jack.e.holland.gr@dartmouth.edu>
2014-11-23 22:59:00 -0500

    Added generalized visualizations and cross validation

--- BioANN.py
+++ BioANN.py
@@ -14,7 +14,7 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import sys
 import json
-from scipy import array, where, argmax
+from scipy import array, where, argmax, shape
 import matplotlib.pyplot as plt
 import re
 from graph_tool.all import *
@@ -24,6 +24,7 @@ from math import log10
 micro_dim = 24 #the number of input nodes
 num_classes = 3
 num_hidden = [48,12] #number of hidden layers
+shapes = [(24, 48), (48, 12)]
 inc_bias = True #to include bias or not
 
 momentum = 0.1 #momentum 
@@ -35,8 +36,6 @@ myError = 10.0
 max_iterations = 1000000 #maximum number of iterations
 num_epochs = 5 #number of ephochs at every iteration
 snapshot = 10 #when to snapshot the ANN
-
-shapes = [(24, 48), (48, 3)]
 
 def load_params():
 	with open('params', 'r') as f_in:
@@ -53,15 +52,16 @@ def load_params():
 	global snapshot
 	micro_dim = int(params['micro_dim']) 
 	num_hidden = params['num_hidden']
+	shapes = [(micro_dim, num_hidden[0])]
+	for i in range(1, len(num_hidden)-1):
+		shapes.append((num_hidden[i], num_hidden[i+1]))
+	shapes.append((num_hidden[-1], 3))
 	inc_bias = bool(params['inc_bias'])
-
 	momentum = float(params['momentum'])
 	weight_decay = float(params['weight_decay'])
 	max_iterations = int(params['max_iterations'])
 	num_epochs = int(params['num_epochs'])
 	snapshot = int(params['snapshot'])
-
-
 
 def extract_data(filename, ds):
 	with open(filename, 'r') as f:
@@ -72,11 +72,11 @@ def extract_data(filename, ds):
 			microarray = [float(i) for i in cells[3:]]
 			ds.addSample(microarray, (label))
 
-def train(filename, start=''):
+def train(data_file, vis_matrix, vis_graph, save_file=''):
 	load_params()
 	#import dataset
 	ds = ClassificationDataSet(micro_dim, 1, nb_classes=num_classes)
-	extract_data(filename, ds)
+	extract_data(data_file, ds)
 	
 	tr, val = ds.splitWithProportion(0.9) #10% validation data
 	#softmax output layer
@@ -84,12 +84,12 @@ def train(filename, start=''):
 	val._convertToOneOfMany()
 	
 	#build network
-	if start == '':
-		ann = buildNetwork(tr.indim, 48, 3, 48, tr.outdim, hiddenclass=SigmoidLayer, recurrent=False, outclass=SoftmaxLayer, bias=inc_bias)
+	if save_file == '':
+		ann = buildNetwork(tr.indim, num_hidden[0], num_hidden[1], tr.outdim, hiddenclass=SigmoidLayer, recurrent=False, outclass=SoftmaxLayer, bias=inc_bias)
 		iteration = 0
 	else:
-		ann = NetworkReader.readFrom(start)
-		match = re.search('([0-9]+)_(?:[0-9]{1,3}).xml', start)
+		ann = NetworkReader.readFrom(save_file)
+		match = re.search('([0-9]+)_(?:[0-9]{1,3}).xml', save_file)
 		if match == None:
 			print 'Net save files should be named I_E.xml, where I is the iteration and E is the rounded error from 0-100'
 			exit(1)
@@ -104,50 +104,39 @@ def train(filename, start=''):
 	while(not done):
 		trainer.trainEpochs(num_epochs)
 		
-		"""nodes = [[], [], []]
-		for c in ann.connections:
-			index = -1
-			if c._name == 'in':
-				index = 0
-			elif c._name == 'hidden0':
-				index = 1
-			elif c._name == 'out':
-				index = 2
-			if index >= 0:
-				nodes[index] = c.inputbuffer.tolist()"""
+		# visualize iteration
+		if vis_matrix or vis_graph:
+			vertices, edges = verts_edges(ann)
+			if vis_matrix:
+				matrix_visualizer(edges)
+			if vis_graph:
+				graph_visualizer(vertices, edges, iteration)
 		
-		"""matrices = [[], []]
-		for mod in ann.modules:
-			for conn in ann.connections[mod]:
-				if mod.name == 'in' and conn.outmod.name == 'hidden0':
-					matrices[0] = array(conn.params).reshape(shapes[0]).tolist()
-				elif mod.name == 'hidden0' and conn.outmod.name == 'out':
-					matrices[1] = array(conn.params).reshape(shapes[1]).tolist()"""
-		
-		"""f, axs = plt.subplots(1, len(matrices), sharey=True)
-		for i in range(len(matrices)):
-			axs[i].imshow(matrices[i], interpolation='none', shape=(24, 12))
-		f.savefig('matrices-%d.png' % iteration)"""
-		#graph_visualizer(nodes, matrices, iteration)
-		
+		# calculate and print error info
 		errors.append(calcError(trainer))
 		variations.append(calcVariation(trainer))
 		print 'iter %d, error %f, variation %f' % (iteration, errors[-1], variations[-1])
-
-		if iteration >= max_iterations - 1:
-			done = True
 		
 		#save every <snapshot> iterations
-		if iteration % snapshot == 0:
+		if iteration % snapshot == -1:
 			file_data = (iteration, int(errors[-1]*100))
 			print 'Saving model %d_%d.xml...' % file_data
 			NetworkWriter.writeToFile(ann, '%d_%d.xml' % file_data)
 		
+		# go to the next iteration if not done
 		iteration = iteration + 1
+		if iteration >= max_iterations:
+			done = True
 	
 	#testing
-	val_error, val_variation = calcError(trainer, dataset=val), calcVariation(trainer, dataset=val)
-	print 'error %f, variation %f' % (val_error, val_variation)
+	val_errors, val_variations = [], []
+	for i in range(5):
+		val_error, val_variation = calcError(trainer, dataset=val), calcVariation(trainer, dataset=val)
+		print 'error %f, variation %f' % (val_error, val_variation)
+		val_errors.append(val_error)
+		val_variations.append(val_variation)
+		tr, val = ds.splitWithProportion(0.9)
+	print 'average error %f, average variation %f' % (np.average(val_errors), np.average(val_variations))
 	
 	#plotting
 	iterations = range(max_iterations)
@@ -163,7 +152,45 @@ def train(filename, start=''):
 	ax2.set_ylabel('variation (L1 error)')
 	for tick in ax2.get_yticklabels():
 		tick.set_color('r')
-	plt.savefig('error-5layer-48-3-48.png')
+	plt.savefig('error-4layer-48-96.png')
+
+def verts_edges(ann):
+	num_layers = len(num_hidden) + 2
+	nodes = []
+	for i in range(num_layers):
+		nodes.append([])
+	for c in ann.connections:
+		index = -1
+		if c._name == 'in':
+			index = 0
+		elif c._name[:-1] == 'hidden':
+			index = 1 + int(c._name[-1])
+		elif c._name == 'out':
+			index = num_layers - 1
+		if index >= 0:
+			nodes[index] = c.inputbuffer.tolist()
+	edges = []
+	for i in range(num_layers - 1):
+		edges.append([])
+	for mod in ann.modules:
+		for conn in ann.connections[mod]:
+			layer1, layer2 = mod.name, conn.outmod.name
+			if layer1 == 'in' and layer2[:-1] == 'hidden':
+				index = 0
+			elif layer1[:-1] == 'hidden' and layer2 == 'out':
+				index = num_layers - 2
+			elif layer1 != 'bias':
+				index = int(layer1[-1])
+			if layer1 != 'bias':
+				print index, shapes, shape(array(conn.params))
+				edges[index] = array(conn.params).reshape(shapes[index]).tolist()
+	return nodes, edges
+
+def matrix_visualizer(edges):
+	f, axs = plt.subplots(1, len(edges), sharey=True)
+	for i in range(len(edges)):
+		axs[i].imshow(edges[i], interpolation='none', shape=shapes[i])
+	f.savefig('matrices-%d.png' % iteration)
 
 def graph_visualizer(vertices, edges, iteration):
 	g = Graph(directed=False)
@@ -172,7 +199,6 @@ def graph_visualizer(vertices, edges, iteration):
 	vcolor = g.new_vertex_property("vector<float>")
 	ecolor = g.new_edge_property("vector<float>")
 	ewidth = g.new_edge_property("float")
-
 	layers_vertices = []
 	for i in range(len(vertices)):
 		layer = vertices[i][0]
@@ -185,7 +211,6 @@ def graph_visualizer(vertices, edges, iteration):
 			vsize[vertex] = layer[j] * 3
 			vcolor[vertex] = [0, 105/255.0, 62/255.0, 1]
 		layers_vertices.append(layer_vertices)
-
 	for i in range(len(edges)):
 		matrix = edges[i]
 		for j in range(len(matrix)):
@@ -195,7 +220,6 @@ def graph_visualizer(vertices, edges, iteration):
 				shade = 1 - vector[k]
 				ecolor[edge] = [shade, shade, shade, 1]
 				ewidth[edge] = 2
-
 	graph_draw(g, vpos, vertex_size=vsize, vertex_fill_color=vcolor, edge_color=ecolor, edge_pen_width=ewidth, bg_color=[1,1,1,1], output_size=(800, 800), output='graph-%d.png' % iteration)
 
 def calcError(trainer, dataset=None):
@@ -227,10 +251,19 @@ def calcVariation(trainer, dataset=None):
             errors.append(error_sum)
     return sum(errors) / len(errors)
 
+def param_true(param):
+	return param == '1' or param == 'true'
+
 if __name__ == '__main__':
-	operation = sys.argv[1]
-	if operation in 'train':
-		train(sys.argv[2])
-	else:
-		train(sys.argv[2], sys.argv[3])
+	num_args = len(sys.argv)
+	if num_args == 1:
+		print 'usage: BioANN.py data_file [visualize_matrix visualize_graph save_file]'
+	data_file, vis_matrix, vis_graph, save_file = sys.argv[1], False, False, ''
+	if num_args >= 3:
+		vis_matrix = param_true(sys.argv[2])
+	if num_args >= 4:
+		vis_graph = param_true(sys.argv[3])
+	if num_args >= 5:
+		save_file = sys.argv[4]
+	train(data_file, vis_matrix, vis_graph, save_file)
 
